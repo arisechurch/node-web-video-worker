247a888158cac2a2f5a8b933ffc7d24a46a3e6e7
Tim Smart <tim@fostle.com>
2014-01-23 09:28:53 +1300

    Add crf to webm
    Signed-off-by: Tim Smart <tim@fostle.com>

--- presets/webm.video.js
+++ presets/webm.video.js
@@ -4,9 +4,10 @@ module.exports = function (ffmpeg) {
     .addOption('-codec:v', 'libvpx')
     .addOption('-quality', 'good')
     .addOption('-cpu-used', '0')
-    .addOption('-qmin', '5')
+    .addOption('-qmin', '0')
     .addOption('-qmax', '44')
     .addOption('-pix_fmt', 'yuv420p')
     .addOption('-r:v', '30')
     .addOption('-vsync', 'vfr')
+    .addOption('-crf', '10')
 }
