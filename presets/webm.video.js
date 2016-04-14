b9e75676c1c84465a894d7d09a4ce59b9ed65b84
Tim Smart <tim.smart@arisechurch.com>
2016-04-14 13:30:04 +1200

    Remove some options from vp8 encoding
    Signed-off-by: Tim Smart <tim.smart@arisechurch.com>

--- presets/webm.video.js
+++ presets/webm.video.js
@@ -2,8 +2,6 @@
 module.exports = function (ffmpeg) {
   ffmpeg
     .addOption('-codec:v', 'libvpx')
-    .addOption('-quality', 'good')
-    .addOption('-cpu-used', '0')
     .addOption('-qmin', '0')
     .addOption('-qmax', '40')
     .addOption('-pix_fmt', 'yuv420p')
